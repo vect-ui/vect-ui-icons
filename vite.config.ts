bdc9c2fa51c60b442628432a85f95014af7861e9
xiehaijie <320974775@qq.com>
2022-12-07 12:04:09 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
 
-
 export default defineConfig({
   build: {
     outDir: `dist`,
